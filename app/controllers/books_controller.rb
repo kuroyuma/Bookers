cb6a3b26c33257a84bc35d5a438640287779116c
kuroyuma <k.1993.yuma.0324@gmail.com>
2021-09-09 14:32:51 +0000

    [sheet]16.5

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,13 @@
 class BooksController < ApplicationController
   def index
-    
+    @book = Book.all
+
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
   end
 
   def show
@@ -8,4 +15,10 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
