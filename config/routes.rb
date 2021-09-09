05fe6631d9caf232b602be469220b29242824833
kuroyuma <k.1993.yuma.0324@gmail.com>
2021-09-09 12:08:27 +0000

    [sheet]14

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'homes/top'
   #get 'books/index'
   #get 'books/show'
   #get 'books/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root :to => 'books#index'
+  root :to => 'homes#top'
   resources :books
 
 end
